5a10784cad08bcbbf60b46ffd129fb891b860702
igomine <igomine@126.com>
2017-05-16 03:32:44 +0000

    1

--- 485test/485test.py
+++ 485test/485test.py
@@ -1,25 +1,31 @@
 import serial
 import time
 from struct import pack, unpack
+import RPi.GPIO as gpio
 
-# rs485tometer = serial.Serial(port='/dev/serial0', baudrate=9600, bytesize=8, stopbits=1, timeout=1)
-# rs485tometer.open()
+gpio.setmode(gpio.BCM)
+gpio.setup(21, gpio.OUT)
+gpio.output(21, gpio.HIGH)
+
+while True:
+    print("a")
+    time.sleep(2)
+    pass
 
 rs485tometer = serial.Serial('/dev/serial0', 9600, timeout=1)
-# rs485tometer.baudrate = 9600
-# rs485tometer.port = '/dev/serial0'
+
 
 if rs485tometer.isOpen() is False:
     rs485tometer.open()
 
 
 cmd1_head = b'UUUU2'
-cmd2_address = b'\x01'
-meter_float = 17.23
+cmd2_address = b'\x03'
+meter_float = 3240.0
 cmd3_position = pack('f', meter_float)
 
 while True:
     rs485tometer.write(cmd1_head)
     rs485tometer.write(cmd2_address)
     rs485tometer.write(cmd3_position)
-    time.sleep(1)
+    time.sleep(3)
