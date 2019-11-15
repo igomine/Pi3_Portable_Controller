60192cbcd0beb4d9df428d55196ebb6ac745ed80
igomine <igomine@126.com>
2019-11-15 03:37:08 +0000

    5.4

--- portable_controller/Portable_raspi_2019_11_13.py
+++ portable_controller/Portable_raspi_2019_11_13.py
@@ -5,6 +5,9 @@
     spi communication between Raspiberry and MCU
         2019.7.22 hzm
 
+        zrd
+
+
 """
 
 import serial
@@ -24,8 +27,9 @@ import os
 import queue
 import random
 
-commandsend = b'UUUU3'  #Pi to MCU command words
-commandrecv = b'UUUU4'  #MCU to Pi command words
+commandsend = b'UUUU3'  # Pi to MCU command words
+commandrecv = b'UUUU4'  # MCU to Pi command words
+
 
 class modbusserver(object):
     def __init__(self, server, slaveid):
@@ -34,23 +38,23 @@ class modbusserver(object):
         self.spi = spidev.SpiDev()
         self.spi.open(0, 0)
         self.spi.mode = 0b01
-        self.spi.max_speed_hz =500000
+        self.spi.max_speed_hz = 500000
         self.spi.cshigh = False
-        self.senddata = [0]*41
-        self.recvdata = [0]*41
-        self.metor_value = [0]*16
-        self.last_metor_value = [0]*16
+        self.senddata = [0] * 41
+        self.recvdata = [0] * 41
+        self.metor_value = [0] * 16
+        self.last_metor_value = [0] * 16
         self.coils_value = None
         self.last_coils_value = None
-        self.led_tmp = [([0]*8) for i in range(4)]
-        self.led = [0]*4
-        self.ai_value = [0]*22
+        self.led_tmp = [([0] * 8) for i in range(4)]
+        self.led = [0] * 4
+        self.ai_value = [0] * 22
         self.last_ai_value = [0] * 22
-        self.ai = [0]*11
-        self.key_value = [0]*4
-        self.last_key_value = [0]*4
-        self.key_tmp = [([0]*8) for i in range(4)]
-        self.key = [0]*32
+        self.ai = [0] * 11
+        self.key_value = [0] * 4
+        self.last_key_value = [0] * 4
+        self.key_tmp = [([0] * 8) for i in range(4)]
+        self.key = [0] * 32
 
         self.slave = self.server.get_slave(self.slaveid)
 
@@ -58,26 +62,26 @@ class modbusserver(object):
         GPIO.setup(7, GPIO.OUT, initial=GPIO.HIGH)
 
     def sendtomcu(self):
-        #add command word "UUUU3"
+        # add command word "UUUU3"
         self.senddata[0:5] = commandsend
-        #get metor from PC
+        # get metor from PC
         self.metor_value = list(self.slave.get_values('HOLDING_REGISTERS', 0, 16))
         print("metor", self.metor_value)
         if (self.metor_value != self.last_metor_value):
             for i in range(0, 16):
-                self.senddata[2*i+5] = self.metor_value[i] & 0xff
-                self.senddata[2*i+5+1] = self.metor_value[i] >> 8
+                self.senddata[2 * i + 5] = self.metor_value[i] & 0xff
+                self.senddata[2 * i + 5 + 1] = self.metor_value[i] >> 8
             self.last_metor_value = copy.copy(self.metor_value)
 
-        #get lamp from PC
+        # get lamp from PC
         self.coils_value = self.slave.get_values('COILS', 0, 32)
         self.led_tmp[0] = self.coils_value[0:8]
         self.led_tmp[1] = self.coils_value[8:16]
         self.led_tmp[2] = self.coils_value[16:24]
         self.led_tmp[3] = self.coils_value[24:32]
-        #change di from list to byte
+        # change di from list to byte
         if (self.coils_value != self.last_coils_value):
-            #chang 8 bit data to one byte
+            # chang 8 bit data to one byte
             for i in range(0, 4):
                 for j in range(0, 8):
                     if self.led_tmp[i][j] == 0x1:
@@ -87,7 +91,7 @@ class modbusserver(object):
                     if j != 7:
                         self.led[i] = self.led[i] >> 1
 
-            #package senddata
+            # package senddata
             self.senddata[37] = self.led[0]
             self.senddata[38] = self.led[1]
             self.senddata[39] = self.led[2]
@@ -95,27 +99,27 @@ class modbusserver(object):
             self.last_coils_value = copy.copy(self.coils_value)
 
         print("senddata = ", self.senddata)
-        #Master start SPI transport
+        # Master start SPI transport
         GPIO.output(7, GPIO.LOW)
         self.recvdata = self.spi.xfer2(self.senddata)
         GPIO.output(7, GPIO.HIGH)
         print("recvdata", self.recvdata)
 
-    #analysis receive data
+    # analysis receive data
     def recvmcu(self):
-        #check command word
+        # check command word
         if (self.recvdata[0:5] == list(commandrecv)):
             for i in range(0, 22):
-                self.ai_value[i] = self.recvdata[i+5]
+                self.ai_value[i] = self.recvdata[i + 5]
             if (self.ai_value != self.last_ai_value):
                 for i in range(0, 11):
-                    self.ai[i] = (self.ai_value[2*i] & 0xff) + (self.ai_value[2*i+1] << 8)
+                    self.ai[i] = (self.ai_value[2 * i] & 0xff) + (self.ai_value[2 * i + 1] << 8)
                     self.slave.set_values('READ_INPUT_REGISTERS', 0, self.ai)
                     self.last_ai_value = copy.copy(self.ai_value)
 
             for i in range(0, 4):
-                self.key_value[i] = self.recvdata[i+27]
-            #change key to list from byte
+                self.key_value[i] = self.recvdata[i + 27]
+            # change key to list from byte
             if (self.key_value != self.last_key_value):
                 self.last_key_value = copy.copy(self.key_value)
                 for i in range(0, 4):
@@ -130,11 +134,12 @@ class modbusserver(object):
                 self.key[8:16] = self.key_tmp[1]
                 self.key[16:24] = self.key_tmp[2]
                 self.key[24:32] = self.key_tmp[3]
-                #package recvdata
+                # package recvdata
                 self.slave.set_values('DISCRETE_INPUTS', 0, self.key)
 
         print("ai = ", self.ai)
         print("key = ", self.key)
+
 
 def main():
     slaveid = 1
@@ -143,12 +148,12 @@ def main():
     GPIO.setup(13, GPIO.OUT, initial=GPIO.HIGH)
 
     try:
-        #get ip address
+        # get ip address
         gw = os.popen("ip -4 route show default").read().split()
         print("gw", gw)
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        #gate is 255.255.255.0
+        # gate is 255.255.255.0
         s.connect((gw[2], 0))
         ipaddr = s.getsockname()[0]
         print("IP:", ipaddr)
@@ -176,6 +181,7 @@ def main():
     finally:
         server.stop()
         GPIO.cleanup()
+
 
 if __name__ == "__main__":
     main()
